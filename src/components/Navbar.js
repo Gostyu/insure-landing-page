06e8b5561f71434527d5e8bef3d22d86d45196b0
Gostyu <cedric.lissossi@gmail.com>
2020-08-24 23:45:20 +0200

    links in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,19 @@ import React from 'react';
 import logo from '../images/logo.svg';
 import hamburgerLogo from '../images/icon-hamburger.svg';
 import '../stylesheets/components/_navbar.scss';
+function Links(){
+    const items = ["How we work","Blog"," Account","View plans"];
+    return <ul class="nav_links">
+       {items.map((item)=>{
+           return <li class="nav_link"><a>item</a></li>
+       })}
+    </ul>
+}
 export default function NavBar (){
 
     return <nav class="nav">
         <img src={logo} alt="" class="nav_brand"></img>
         <img src={hamburgerLogo} alt=""></img>
+        <Links></Links>
     </nav>
 }
