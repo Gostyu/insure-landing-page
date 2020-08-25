7f3cbef048222b1080c65ff51da2e16d88206bff
Gostyu <cedric.lissossi@gmail.com>
2020-08-25 20:57:39 +0200

    content mobile-first finished

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,5 +2,5 @@ import '../stylesheets/components/_button.scss';
 import React from 'react';
 
 export default function Button({text}){
-return <button className="headerBody_btn headerBody_btn--center btn">{text}</button>
+return <button className="headerBody_btn headerBody_btn--center  contentSection_btn contentSection_btn--center btn">{text}</button>
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,8 @@
 import React, { Children } from 'react';
 import '../stylesheets/layouts/_content.scss';
 import contents from '../models/content_data.js';
-
+import Button from '../components/Button';
+import bgPatternHww from '../images/bg-pattern-how-we-work-mobile.svg';
 const ContentChild = (props)=>{
     return  <div className="contentChild">
         <img className="contentChild_img" src={props.img} alt=""></img>
@@ -19,6 +20,10 @@ export default function Content(){
         <div className="contentLine"></div>
         <h1 className="contentTitle">{titleContent}</h1>
         {contentChildItems}
-        
+        <section className="contentSection">
+            <img className="contentSectionImg" src={bgPatternHww} alt=""></img>
+            <h1 className="contentSectionText">Find out more about how we work</h1>
+            <Button text="How we work"></Button>
+        </section>
     </main>;
 }
