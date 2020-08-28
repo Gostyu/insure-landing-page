b9b25dd1aeae06b2dd48cf84b751f6eae67be426
Gostyu <cedric.lissossi@gmail.com>
2020-08-29 01:34:33 +0200

    Updates

---
+++ precache-manifest.6e5a53d07e1e0067fe64e9590e7b9cce.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d4598648a285d3d0cb0b54e3f0cad7",
+    "url": "/insure-landing-page/index.html"
+  },
+  {
+    "revision": "89ebf6c45faa0aed4b8d",
+    "url": "/insure-landing-page/static/css/main.9c9e27c9.chunk.css"
+  },
+  {
+    "revision": "e490cac59a70c3660601",
+    "url": "/insure-landing-page/static/js/2.351137fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/insure-landing-page/static/js/2.351137fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ebf6c45faa0aed4b8d",
+    "url": "/insure-landing-page/static/js/main.1ab3f11b.chunk.js"
+  },
+  {
+    "revision": "223baf2318f7ad551d90",
+    "url": "/insure-landing-page/static/js/runtime-main.dd5516c1.js"
+  },
+  {
+    "revision": "1586c300d23ec651816b4fbe776ce89a",
+    "url": "/insure-landing-page/static/media/bg-pattern-footer-desktop.1586c300.svg"
+  },
+  {
+    "revision": "c33e3a526c0d772e3611831d1e3fc838",
+    "url": "/insure-landing-page/static/media/bg-pattern-footer-mobile.c33e3a52.svg"
+  },
+  {
+    "revision": "a4de14b0ee74ee9f31076b875f4ef78e",
+    "url": "/insure-landing-page/static/media/bg-pattern-how-we-work-mobile.a4de14b0.svg"
+  },
+  {
+    "revision": "562149a81a4a5d64fcb7309999bdbcf9",
+    "url": "/insure-landing-page/static/media/bg-pattern-intro-left-mobile.562149a8.svg"
+  },
+  {
+    "revision": "ef3d23bd373b877c0f109cf00fccdd0d",
+    "url": "/insure-landing-page/static/media/bg-pattern-intro-right-mobile.ef3d23bd.svg"
+  },
+  {
+    "revision": "b9dc0b5c6bd0cf4ddcd5ce9f3ad16897",
+    "url": "/insure-landing-page/static/media/icon-affordable-prices.b9dc0b5c.svg"
+  },
+  {
+    "revision": "6fd0e32479e7d8af8e471d2a34244e93",
+    "url": "/insure-landing-page/static/media/icon-facebook.6fd0e324.svg"
+  },
+  {
+    "revision": "d98c1990131df21b2dc25cd8c1382819",
+    "url": "/insure-landing-page/static/media/icon-hamburger.d98c1990.svg"
+  },
+  {
+    "revision": "48a204c8175de66e9c8f1b9207cc77eb",
+    "url": "/insure-landing-page/static/media/icon-instagram.48a204c8.svg"
+  },
+  {
+    "revision": "b264095b9d9862f3cc93d449508b65fd",
+    "url": "/insure-landing-page/static/media/icon-people-first.b264095b.svg"
+  },
+  {
+    "revision": "275a65a2fef7bff622ba4fb6e7aa57eb",
+    "url": "/insure-landing-page/static/media/icon-pinterest.275a65a2.svg"
+  },
+  {
+    "revision": "3cc5dc39b6297427126b6a7389e9b76d",
+    "url": "/insure-landing-page/static/media/icon-snappy-process.3cc5dc39.svg"
+  },
+  {
+    "revision": "6eef62ff8487d0c9808c68e74d68d9c8",
+    "url": "/insure-landing-page/static/media/icon-twitter.6eef62ff.svg"
+  },
+  {
+    "revision": "09a91c1a381a6fc1ecb8512490b74a12",
+    "url": "/insure-landing-page/static/media/image-intro-mobile.09a91c1a.jpg"
+  },
+  {
+    "revision": "1bc452ac6f5856ef32a25e4519a4db91",
+    "url": "/insure-landing-page/static/media/logo.1bc452ac.svg"
+  }
+]);
