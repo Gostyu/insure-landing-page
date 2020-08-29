8eedbbe9fe66f2adfc5eef5e91bc6c2e0445a500
Gostyu <cedric.lissossi@gmail.com>
2020-08-29 03:16:44 +0200

    navbar modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from '../images/logo.svg';
 import hamburgerLogo from '../images/icon-hamburger.svg';
 import '../stylesheets/components/_navbar.scss';
+import Button from './Button';
 function Links(){
-    const items = ["How we work","Blog"," Account","View plans"];
+    const items = ["How we work","Blog"," Account"];
     return <ul className="nav_links">
        {items.map((item)=>{
            return <li className="nav_link"><a href={"#"+item}>{item}</a></li>
        })}
+       <Button className="btn nav_links_btn">View plans</Button>
     </ul>
 }
 export default function NavBar (){
